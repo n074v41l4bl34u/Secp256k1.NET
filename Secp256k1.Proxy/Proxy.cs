e22af34ea37ae972c70622c8118ff90168f0589e
Josh Lang <josh@joshlang.ca>
2015-11-02 13:16:15 -0800

    Improved performance by switching from reflection to delegates

--- Secp256k1.Proxy/Proxy.cs
+++ Secp256k1.Proxy/Proxy.cs
@@ -9,39 +9,36 @@ namespace Secp256k1
 {
     public static class Proxy
     {
-        public static readonly bool Ok = Init();
-
-        static MethodInfo VerifyMethod;
-        static MethodInfo SignMethod;
-        static MethodInfo SignCompactMethod;
-        static MethodInfo RecoverKeyFromCompactMethod;
-        static MethodInfo VerifyPrivateKeyMethod;
-        static MethodInfo GetPublicKeyMethod;
-
-        static bool Init()
+        static Type signaturesType;
+        static Type SignaturesType
         {
-            Assembly a = Assembly.LoadFrom("Secp256k1." + (IntPtr.Size == 4 ? "x86" : "x64") + ".dll");
-            var t = a.GetType("Secp256k1.Signatures");
-            VerifyMethod = t.GetRuntimeMethod("Verify", new Type[] { typeof(byte[]), typeof(byte[]), typeof(byte[]) });
-            SignMethod = t.GetRuntimeMethod("Sign", new Type[] { typeof(byte[]), typeof(byte[]) });
-            SignCompactMethod = t.GetRuntimeMethod("SignCompact", new Type[] { typeof(byte[]), typeof(byte[]), typeof(int).MakeByRefType() });
-            RecoverKeyFromCompactMethod = t.GetRuntimeMethod("RecoverKeyFromCompact", new Type[] { typeof(byte[]), typeof(byte[]), typeof(int), typeof(bool) });
-            VerifyPrivateKeyMethod = t.GetRuntimeMethod("VerifyPrivateKey", new Type[] { typeof(byte[]) });
-            GetPublicKeyMethod = t.GetRuntimeMethod("GetPublicKey", new Type[] { typeof(byte[]), typeof(bool) });
-            return VerifyMethod != null && SignMethod != null && SignCompactMethod != null && RecoverKeyFromCompactMethod != null && VerifyPrivateKeyMethod != null && GetPublicKeyMethod != null;
+            get
+            {
+                if (signaturesType == null)
+                {
+                    Assembly a = Assembly.LoadFrom("Secp256k1." + (IntPtr.Size == 4 ? "x86" : "x64") + ".dll");
+                    signaturesType = a.GetType("Secp256k1.Signatures");
+                }
+                return signaturesType;
+            }
         }
 
-        public static bool Verify(byte[] message, byte[] signature, byte[] publicKey) => (bool)VerifyMethod.Invoke(null, new object[] { message, signature, publicKey });
-        public static byte[] Sign(byte[] message, byte[] privateKey) => (byte[])SignMethod.Invoke(null, new object[] { message, privateKey });
-        public static byte[] SignCompact(byte[] message, byte[] privateKey, out int recoveryId)
-        {
-            object[] args = new object[] { message, privateKey, 0 };
-            byte[] result = (byte[])SignCompactMethod.Invoke(null, args);
-            recoveryId = (int)args[2];
-            return result;
-        }
-        public static byte[] RecoverKeyFromCompact(byte[] message, byte[] signature, int recoveryId, bool compressed) => (byte[])RecoverKeyFromCompactMethod.Invoke(null, new object[] { message, signature, recoveryId, compressed });
-        public static bool VerifyPrivateKey(byte[] privateKey) => (bool)VerifyPrivateKeyMethod.Invoke(null, new object[] { privateKey });
-        public static byte[] GetPublicKey(byte[] privateKey, bool compressed) => (byte[])GetPublicKeyMethod.Invoke(null, new object[] { privateKey, compressed });
+        public delegate bool VerifyPrivateKeyDelegate(byte[] privateKey);
+        public static VerifyPrivateKeyDelegate VerifyPrivateKey = (VerifyPrivateKeyDelegate)Delegate.CreateDelegate(typeof(VerifyPrivateKeyDelegate), SignaturesType.GetRuntimeMethod("VerifyPrivateKey", new Type[] { typeof(byte[]) }));
+
+        public delegate bool VerifyDelegate(byte[] message, byte[] signature, byte[] publicKey);
+        public static VerifyDelegate Verify = (VerifyDelegate)Delegate.CreateDelegate(typeof(VerifyDelegate), SignaturesType.GetRuntimeMethod("Verify", new Type[] { typeof(byte[]), typeof(byte[]), typeof(byte[]) }));
+
+        public delegate byte[] SignDelegate(byte[] message, byte[] privateKey);
+        public static SignDelegate Sign = (SignDelegate)Delegate.CreateDelegate(typeof(SignDelegate), SignaturesType.GetRuntimeMethod("Sign", new Type[] { typeof(byte[]), typeof(byte[]) }));
+
+        public delegate byte[] SignCompactDelegate(byte[] message, byte[] privateKey, out int recoveryId);
+        public static SignCompactDelegate SignCompact = (SignCompactDelegate)Delegate.CreateDelegate(typeof(SignCompactDelegate), SignaturesType.GetRuntimeMethod("SignCompact", new Type[] { typeof(byte[]), typeof(byte[]), typeof(int).MakeByRefType() }));
+
+        public delegate byte[] RecoverKeyFromCompactDelegate(byte[] message, byte[] signature, int recoveryId, bool compressed);
+        public static RecoverKeyFromCompactDelegate RecoverKeyFromCompact = (RecoverKeyFromCompactDelegate)Delegate.CreateDelegate(typeof(RecoverKeyFromCompactDelegate), SignaturesType.GetRuntimeMethod("RecoverKeyFromCompact", new Type[] { typeof(byte[]), typeof(byte[]), typeof(int), typeof(bool) }));
+
+        public delegate byte[] GetPublicKeyDelegate(byte[] privateKey, bool compressed);
+        public static GetPublicKeyDelegate GetPublicKey = (GetPublicKeyDelegate)Delegate.CreateDelegate(typeof(GetPublicKeyDelegate), SignaturesType.GetRuntimeMethod("GetPublicKey", new Type[] { typeof(byte[]), typeof(bool) }));
     }
 }
